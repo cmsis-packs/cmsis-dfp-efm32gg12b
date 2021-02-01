ce6248d8a5deed0e659aff6c1fe877bf25f566e4
Rafael Silva <silvagracarafael@gmail.com>
2021-02-01 17:47:43 +0000

    template

---
+++ cmsis-deploy.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+name=cmsis-dfp-template
+vendor=template
+version=0.0
+source_url=
+
+build_dir=cmsis_build
+deploy_dir=cmsis_deploy
+
+prepare() {
+    echo "preparing..." 
+    
+    if [ -z "$build_dir" ]
+    then
+        echo " var\$build_dir is empty"
+        exit
+    fi
+
+    if [ -z "$deploy_dir" ]
+    then
+        echo "var \$deploy_dir is empty"
+        exit
+    fi
+    
+    mkdir -p $build_dir
+    mkdir -p $deploy_dir
+
+    if [ "$(ls -A $build_dir)" ]; then
+        echo "Directory $build_dir is not Empty"
+        echo "Running \"rm -rf $build_dir/*\""
+        rm -rf $build_dir/*
+    fi
+
+    if [ "$(ls -A $deploy_dir)" ]; then
+        echo "Directory $deploy_dir is not Empty"
+        echo "Running \"rm -rf $deploy_dir/*\""
+        rm -rf $deploy_dir/*
+    fi
+    
+    if [ -f "$build_dir/version" ]; then
+        echo "File \"$build_dir/version\"  already exists"
+        echo "Running \"rm $build_dir/version\""
+        rm $build_dir/versionname
+    fi
+
+    touch $build_dir/version
+
+    echo $version >> $build_dir/version
+}
+
+download() {
+    echo "downloading..."
+    curl -o $build_dir/pack-src.pack $source_url
+}
+
+extract() {
+    echo "extracting..."
+    unzip $build_dir/pack-src.pack -d $build_dir
+}
+
+deploy() {
+    echo "deploying..."
+    cp -r $build_dir/Device $deploy_dir
+    cp -r $build_dir/Documents $deploy_dir
+    cp -r $build_dir/Flash $deploy_dir
+    cp -r $build_dir/SVD $deploy_dir
+}
+
+prepare
+download
+extract
+deploy
