d91a9856f2c5fae634db00072934d05089fb12cd
Rafael Silva <perigoso@riseup.net>
2021-09-02 20:21:08 +0100

    v3.2.0

--- cmsis-deploy.sh
+++ cmsis-deploy.sh
@@ -2,7 +2,7 @@
 
 name=cmsis-dfp-efm32gg12b
 vendor=SiliconLabs
-version=3.1.0
+version=3.2.0
 source_url=https://www.silabs.com/documents/public/cmsis-packs/$vendor.GeckoPlatform_EFM32GG12B_DFP.$version.pack
 
 build_dir='cmsis_build'
